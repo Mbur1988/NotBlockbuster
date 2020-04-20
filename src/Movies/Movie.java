e37d64311b66d6a6b55ea5e66f05d092ee1498b6
Mark Burton <markburton88@live.com>
2020-04-20 17:32:24 +1000

    Project created

---
+++ src/Movies/Movie.java
@@ -0,0 +1,15 @@
+package Movies;
+
+import java.time.*;
+
+public class Movie {
+
+    private String title;
+    private String[] starring;
+    private String director;
+    private Duration duration;
+    private String genre;
+    private String classification;
+    private LocalDate release_date;
+
+}
